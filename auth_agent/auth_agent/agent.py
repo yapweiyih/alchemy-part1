5253bf17ea89a77faf558ceac2d9770545154f86
yapweiyih <weiyih@google.com>
2025-09-27 09:52:39 +0800

    chore: cleanup

--- auth_agent/auth_agent/agent.py
+++ auth_agent/auth_agent/agent.py
@@ -12,14 +12,6 @@ from google.cloud import secretmanager
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
-
-def get_secret(secret_id, project_id):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("UTF-8")
-
-
 logger = logging.getLogger(__name__)
 
 load_dotenv()
